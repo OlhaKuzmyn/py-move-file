4558a67778cdbed3fcbd9b327b4badc931a30d5b
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-04-01 21:46:47 +0300

    added check for mv command

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ import os
 def move_file(command: str) -> None:
     try:
         cmnd, file_name, new_path = command.split(" ")
-        new_path_dir = os.path.dirname(new_path)
-        if new_path_dir:
-            if not os.path.exists(new_path_dir):
-                os.makedirs(new_path_dir)
-        os.rename(file_name, new_path)
+        if cmnd == "mv":
+            new_path_dir = os.path.dirname(new_path)
+            if new_path_dir:
+                if not os.path.exists(new_path_dir):
+                    os.makedirs(new_path_dir)
+            os.rename(file_name, new_path)
     except ValueError:
         pass
