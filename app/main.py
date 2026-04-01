3764f821b2a751ade828f42aab49b784092bec5b
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-04-01 20:53:09 +0300

    solved

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    cmnd, file_name, new_path = command.split(" ")
+    if "/" in new_path:
+        new_path_dir = new_path[:new_path.rfind("/")]
+        if not os.path.exists(new_path_dir):
+            os.makedirs(new_path_dir)
+    os.rename(file_name, new_path)
